abd911a320714184bbc5714fe195083891fb2419
yancesiusssssssss <96276937+yancesiusssssssss@users.noreply.github.com>
2022-01-05 21:32:33 -0800

    Add files via upload

---
+++ Alpro1Biodata.java
@@ -0,0 +1,25 @@
+
+ 
+package alpro.pkg1.biodata;
+
+
+public class Alpro1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("NAMA LENGKAP = YANCESIUS SALINDING");
+      System.out.println("TEMPAT TANGGAL LAHIR = TORAJA 26 JULI 2003");
+       System.out.println("AGAMA = KRISTEN");
+        System.out.println("ALAMAT = BUNGIN TANA TORAJA");
+        System.out.println("NOMOR HANDPHONE = 082188175844");
+        System.out.println("PROGRAM STUDI = TEKNIK INFORMATIKA");
+        System.out.println("FAKULTAS = FTI");
+        System.out.println("UNIVERSITAS = UAJM");
+        System.out.println("ASAL SMA = SMA KRISTEN MAKALE ");
+        System.out.println("alasan memilih UAJM = KARENA INGIN MEMULAI HIDUP BARU DISINI");
+        
+    }
+    
+}
